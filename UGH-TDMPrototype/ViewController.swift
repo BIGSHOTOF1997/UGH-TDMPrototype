a960d4af9df87f44ab20cd197afc1cc941cd1e8c
mattsoh <67374564+mattsoh@users.noreply.github.com>
2023-09-17 09:12:11 +0100

    Add files via upload

---
+++ UGH-TDMPrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UGH-TDMPrototype
+//
+//  Created by Matthew Soh on 16/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
